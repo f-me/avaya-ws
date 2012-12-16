2cc8e8dd20e48d88c7dcf0822259fe24380da063
Max Taldykin <jorpic@gmail.com>
2012-12-17 03:45:39 +0400

    Extensive logging

--- src/Main.hs
+++ src/Main.hs
@@ -14,6 +14,7 @@ import System.Environment (getArgs)
 import Control.Concurrent
 import Control.Concurrent.STM
 import Network.WebSockets
+import System.Posix.Syslog
 import qualified Data.Configurator as Cfg
 
 import Avaya.Actions
@@ -42,7 +43,7 @@ main = getArgs >>= \case
 realMain :: FilePath -> IO ()
 realMain config = do
   c <- Cfg.load [Cfg.Required config]
-  cfg <- Config
+  cfg@(Config{..}) <- Config
       <$> Cfg.require c "listen-port"
       <*> Cfg.require c "aes-addr"
       <*> Cfg.require c "aes-port"
@@ -50,9 +51,10 @@ realMain config = do
       <*> Cfg.require c "aes-pass"
       <*> Cfg.require c "aes-switch"
 
-  connMap <- newTVarIO Map.empty
-  runServer "0.0.0.0" (listenPort cfg)
-    $ rqHandler cfg connMap
+  withSyslog "avaya-ws" [PID] USER $ do
+    syslog Notice $ "Starting server on port " ++ show listenPort
+    connMap <- newTVarIO Map.empty
+    runServer "0.0.0.0" listenPort $ rqHandler cfg connMap
 
 
 type AvayaMap = TVar (Map.Map (Text,Text) (LoopHandle,MonitoringHandle))
@@ -60,85 +62,115 @@ type AvayaMap = TVar (Map.Map (Text,Text) (LoopHandle,MonitoringHandle))
 
 chkRequestPath :: Request -> WebSockets Hybi00 (Text,Text)
 chkRequestPath rq
-  = case T.splitOn "/" . T.decodeUtf8 $ requestPath rq of
+  = case T.splitOn "/" rqPath of
     ["", "avaya", ext, pwd] -> return (ext, pwd)
-    _ -> rejectRequest rq "401"
+    _ -> do
+      liftIO $ syslog Warning $ "Invalid request: " ++ show rqPath
+      rejectRequest rq "401"
+  where
+    rqPath = T.decodeUtf8 $ requestPath rq
+
+
+getSession
+  :: AvayaMap -> Config -> Text -> Text
+  -> WebSockets Hybi00 (Either String (LoopHandle,MonitoringHandle))
+getSession cMapVar cfg ext pwd = do
+  cMap <- liftIO $ readTVarIO cMapVar
+  -- check if session for (ext,pwd) already exists
+  case Map.lookup (ext,pwd) cMap of
+    Just hm -> return $ Right hm
+    Nothing -> liftIO -- create new session
+      $ startMonitoring cfg ext pwd
+        >>= \case
+          Left err -> return $ Left err
+          Right (h,m) -> do -- update session map
+            atomically $ writeTVar cMapVar -- FIXME: race condition
+              $! Map.insert (ext,pwd) (h,m) cMap
+            return $ Right (h,m)
 
 
 rqHandler :: Config -> AvayaMap -> Request -> WebSockets Hybi00 ()
 rqHandler cfg cMapVar rq = do
   (ext,pwd) <- chkRequestPath rq
 
-  cMap <- liftIO $ readTVarIO cMapVar
-  Right (h,m) <- case Map.lookup (ext,pwd) cMap of
-    Just hm -> return $ Right hm
-    Nothing -> startMonitoring cfg ext pwd rq
-  liftIO $ atomically
-    $ writeTVar cMapVar $! Map.insert (ext,pwd) (h,m) cMap
+  (h,m) <- getSession cMapVar cfg ext pwd
+        >>= either
+          (\err -> liftIO (syslog Warning err) >> rejectRequest rq "501")
+          return
 
   acceptRequest rq
   s <- getSink
-  liftIO $ attachObserver h $ evHandler s
+  o <- liftIO $ attachObserver h $ evHandler s
 
   let loop
         = receive >>= \case
-          DataMessage (Text t) -> runCommand h m t
+          DataMessage (Text t) -> liftIO $ runCommand h m t
           _ -> return () -- FIXME: log
 
   let shutdown e = liftIO $ do
-        putStrLn $ "Sutdown session " ++ show (sessionId m)
-        atomically $ modifyTVar' cMapVar $ Map.delete (ext,pwd)
-        stopDeviceMonitoring h m
-        shutdownLoop h
+        syslog Notice $ "WsError catched: " ++ show e
+--        detachObserver h o
 
   catchWsError (forever loop) shutdown
 
 
-runCommand h m t
-  = case L.split ':' t of
-      ["dial", number] -> liftIO $ do
-        sendRequestSync h
-          $ Rq.SetHookswitchStatus
-            {acceptedProtocol = actualProtocolVersion m
-            ,device = deviceId m
-            ,hookswitchOnhook = False
-            }
-        dialNumber h (actualProtocolVersion m) (deviceId m) (L.unpack number)
+runCommand :: LoopHandle -> MonitoringHandle -> L.ByteString -> IO ()
+runCommand h m t = do
+  syslog Notice $ "User command: " ++ show (t, deviceId m)
+  case L.split ':' t of
+    ["dial", number] -> do
+      sendRequestSync h
+        $ Rq.SetHookswitchStatus
+          {acceptedProtocol = actualProtocolVersion m
+          ,device = deviceId m
+          ,hookswitchOnhook = False
+          }
+      dialNumber h (actualProtocolVersion m) (deviceId m) (L.unpack number)
 
-      ["acceptCall"]
-        -> liftIO $ sendRequestAsync h
-          $ Rq.SetHookswitchStatus
-            {acceptedProtocol = actualProtocolVersion m
-            ,device = deviceId m
-            ,hookswitchOnhook = False
-            }
-      _ -> return ()
+    ["acceptCall"] -> do
+      sendRequestAsync h
+        $ Rq.SetHookswitchStatus
+          {acceptedProtocol = actualProtocolVersion m
+          ,device = deviceId m
+          ,hookswitchOnhook = False
+          }
+    _ -> return ()
 
 
-evHandler ws = \case
+evHandler :: Sink Hybi00 -> LoopEvent -> IO ()
+evHandler ws ev = case ev of
   AvayaRsp rsp -> case rsp of
-    Rs.RingerStatusEvent{..} -> sendSink ws $ textData $ L.fromChunks
-      ["{\"type\":\"ringer\",\"ringer\":\""
-      ,T.encodeUtf8 ringMode
-      ,"\"}"
-      ]
-    Rs.DisplayUpdatedEvent{..} -> sendSink ws $ textData $ L.fromChunks
-      ["{\"type\":\"display\",\"display\":\""
-      ,T.encodeUtf8 contentsOfDisplay
-      ,"\"}"
-      ]
+    Rs.RingerStatusEvent{..} -> do
+      syslog Notice $ show rsp
+      sendSink ws $ textData $ L.fromChunks
+        ["{\"type\":\"ringer\",\"ringer\":\""
+        ,T.encodeUtf8 ringMode
+        ,"\"}"
+        ]
+    Rs.DisplayUpdatedEvent{..} -> do
+      syslog Notice $ show rsp
+      sendSink ws $ textData $ L.fromChunks
+        ["{\"type\":\"display\",\"display\":\""
+        ,T.encodeUtf8 contentsOfDisplay
+        ,"\"}"
+        ]
     _ -> return ()
   _ -> return ()
 
 
--- startMonitoring :: Config -> Text -> Text -> Int -> Char
-startMonitoring (Config{..}) ext pwd rq
-  = (liftIO $ startMessageLoop aesAddr aesPort)
+startMonitoring
+  :: Config -> Text -> Text
+  -> IO (Either String (LoopHandle,MonitoringHandle))
+startMonitoring (Config{..}) ext pwd
+  = startMessageLoop aesAddr aesPort
     >>= \case
-      Left _ -> rejectRequest rq "Can't start session"
-      Right h ->
-        (liftIO $ startDeviceMonitoring h aesUser aesPass aesSwitch ext pwd)
+      Left err -> return $ Left "Can't start session"
+      Right h  ->
+        startDeviceMonitoring h aesUser aesPass aesSwitch ext pwd
           >>= \case
-            Left _ -> rejectRequest rq "Can't register device"
-            Right m -> return $ Right (h,m)
-        -- liftIO $ attachObserver h print
+            Left err -> do
+              shutdownLoop h
+              return $ Left $ "Can't register device: " ++ show err
+            Right m  -> do
+              syslog Notice $ "Just started new session for " ++ show (ext,pwd)
+              return $ Right (h,m)
